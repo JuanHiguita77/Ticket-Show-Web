824d2ee958485dd6b7ddc0be1cdd578f2ca4293d
Juan Fernando <59887137+JuanHiguita77@users.noreply.github.com>
2024-05-22 16:11:49 -0500

    Delete src directory

--- src/main/java/com/riwi/ticketShowWeb/domain/repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.riwi.ticketShowWeb.domain.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.riwi.ticketShowWeb.domain.entities.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long>
-{}
