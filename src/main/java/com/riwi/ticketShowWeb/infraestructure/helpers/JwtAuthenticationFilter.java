2b8c93e971c17cbc67e66945f67f0492781b51a9
Juan Fernando <59887137+JuanHiguita77@users.noreply.github.com>
2024-05-22 16:11:18 -0500

    Backend Updated

---
+++ src/main/java/com/riwi/ticketShowWeb/infraestructure/helpers/JwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.riwi.ticketShowWeb.infraestructure.helpers;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private final JwtService jwtService;
+
+    @Autowired
+    @Qualifier("CustomUserDetailsService")
+    private final UserDetailsService userDetailsService;
+
+ 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    final String authHeader = getTokenFromRequest(request);
+
+    if (StringUtils.hasLength(authHeader) && authHeader.startsWith("Bearer ") && SecurityContextHolder.getContext().getAuthentication() == null) {
+        String token = authHeader.substring(7);
+        String email = jwtService.getEmailFromToken(token);
+
+        if (email != null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+            
+            if (jwtService.isTokenValid(token, userDetails)) {
+                // Extraer el rol del token y añadirlo a las autoridades
+                String role = jwtService.getRoleFromToken(token);
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, Collections.singletonList(new SimpleGrantedAuthority(role)));
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+    }
+
+    filterChain.doFilter(request, response);
+}
+
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        return request.getHeader(HttpHeaders.AUTHORIZATION);
+    }
+}
